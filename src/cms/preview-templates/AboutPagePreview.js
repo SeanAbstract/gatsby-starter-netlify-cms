ea5352ce5a0650500877006a0e9c9f048fee1a68
Sean Urgel <fluttertraining@gmail.com>
2019-10-15 23:45:54 +0800

    try another cmd previrw config

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -4,18 +4,15 @@ import PropTypes from 'prop-types'
 import {AboutPageTemplate} from '../../templates/about-page'
 
 const AboutPagePreview = ({entry, widgetFor}) => {
-  const entryTabs = entry.getIn(['data', 'tabs'])
-  const tabs = entryTabs ? entryTabs.toJS() : []
+  const data = entry.getIn(['data']).toJS()
+
+  console.log(data)
 
   return (
     <AboutPageTemplate
-      mainpitch={{
-        title: entry.getIn(['data', 'mainpitch', 'title']),
-        description: entry.getIn(['data', 'mainpitch', 'description']),
-        secondDescription: entry.getIn(['data', 'mainpitch', 'secondDescription']),
-      }}
-      tabs={tabs}
-      headerImage={entry.getIn(['data', 'headerImage'])}
+      mainpitch={data.mainpitch || {}}
+      tabs={data.tabs || []}
+      headerImage={data.headerImage}
     />
   )
 }
