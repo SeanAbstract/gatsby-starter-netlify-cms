0773e295333b569961cd791219901e2fc4b5313f
Sean Urgel <fluttertraining@gmail.com>
2019-10-22 13:07:01 +0800

    add comments and downloadNow section

--- src/templates/contact-page/index.js
+++ src/templates/contact-page/index.js
@@ -16,9 +16,33 @@ function ContactPageTemplate(props: Props) {
       <SharedJumbotron headerImage={props.headerImage} title="Contact" description="Here to help" />
 
       <section className="customer-service-section">
-        <div className="row">
-          <div className="col-md-6 d-flex ">
-            <h3>Customer Service</h3>
+        <div className="row h-100">
+          <div className="col-md-6  w-75 d-flex justify-content-center align-items-center flex-column">
+            <div className="mx-auto">
+              <div className="row w-100">
+                <div className="col text-left">
+                  <h3>Customer Service</h3>
+                </div>
+              </div>
+              <div className="row w-100">
+                <div className="col-sm-6">
+                  <p className="lead">Global (En/Ch)</p>
+                  <p className="mb-0">NZ Trading Hours time</p>
+                  <p className="mb-0">10:00 - 16:00</p>
+                  <p className="mb-0">
+                    <strong>+649985377197</strong>{' '}
+                  </p>
+                </div>
+                <div className="col-sm-6">
+                  <p className="lead">Global (En/Ch)</p>
+                  <p className="mb-0">NZ Trading Hours time</p>
+                  <p className="mb-0">10:00 - 16:00</p>
+                  <p className="mb-0">
+                    <strong>+649985377197</strong>{' '}
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div
             className="col-md-6"
