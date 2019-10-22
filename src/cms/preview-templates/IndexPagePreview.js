e7c66af8ed8eba789731ab599028d25c6deb0f83
Sean Urgel <fluttertraining@gmail.com>
2019-10-22 23:00:19 +0800

    fix the cms functionality for chinese and english pages final

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -5,7 +5,7 @@ import {IndexPageTemplate} from '../../templates/index-page'
 
 const IndexPagePreview = ({entry, getAsset}) => {
   const data = entry.getIn(['data']).toJS()
-
+  console.log(entry)
   console.log(data)
 
   return (
