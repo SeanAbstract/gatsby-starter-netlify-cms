8e95fb49eccca5f0f5344f2837a64f1404e3c219
Sean Urgel <fluttertraining@gmail.com>
2019-10-29 23:10:41 +0800

    change favicon and metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,11 +18,6 @@ const TemplateWrapper = ({children}) => {
         <meta name="description" content={description} />
 
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
-        />
-        <link
           rel="icon"
           type="image/png"
           href={`${withPrefix('/')}img/favicon-32x32.png`}
@@ -35,17 +30,12 @@ const TemplateWrapper = ({children}) => {
           sizes="16x16"
         />
 
-        <link
-          rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
-          color="#ff4400"
-        />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content={`${withPrefix('/')}img/og-image.jpg`} />
+        <meta property="og:image" content={`${withPrefix('/')}img/logo.jpg`} />
       </Helmet>
       <Header />
       <StyledMain>{children}</StyledMain>
