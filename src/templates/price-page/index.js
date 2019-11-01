e48535058b58cbcd1029dce5cb9c38569bdf1719
Ricky Boyce <boycce@gmail.com>
2019-11-01 13:23:36 +1300

    Fixed pricing modules

--- src/templates/price-page/index.js
+++ src/templates/price-page/index.js
@@ -104,7 +104,7 @@ export function PricePageTemplate({
 
       <section>
         <div className="container py-5">
-          <div className="row">
+          <div className="row pricing-tables">
             <div className="col-md-10 mx-auto text-center">
               <h3 className="section-leading-text mb-5">{mainPitch}</h3>
 
@@ -112,12 +112,14 @@ export function PricePageTemplate({
                 <div className="col-md-6 d-flex flex-column mb-3">
                   <div
                     onClick={() => setCurrentPackage('lowCommissionPackage')}
-                    className="price-page-h4-wrapper text-center text-primary pb-2 mb-3"
+                    className="price-page-h4-wrapper text-center text-primary mb-4"
                     style={{borderBottom: '2px solid #006FBB'}}
                   >
                     <h4>Low Comission Package</h4>
                   </div>
+                  <div class="pb-3"></div>
                   <div className="card">
+                    <div class="card-type-top">HK$</div>
                     <img
                       className="card-img-top"
                       width="100%"
@@ -163,21 +165,24 @@ export function PricePageTemplate({
                             {packages[currentPackage].hk.financingInterestRate.title}
                           </h5>
                         </div>
-                        <div className="col-5 text-right">
-                          <p>3.983% minimum</p>
-                        </div>
+                        <p className="col-5 text-right">
+                          <strong className="text-primary">3.983%</strong> minimum
+                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
+
                 <div className="col-md-6">
                   <div
                     onClick={() => setCurrentPackage('lowInterestRatePackage')}
-                    className="price-page-h4-wrapper text-center pb-2 mb-3"
+                    className="price-page-h4-wrapper text-center mb-4"
                   >
-                    <h4>Low Comission Package</h4>
+                    <h4>Low Interest Rate Package</h4>
                   </div>
+                  <div class="pb-3"></div>
                   <div className="card">
+                    <div class="card-type-top">US$</div>
                     <img
                       className="card-img-top"
                       width="100%"
@@ -195,9 +200,9 @@ export function PricePageTemplate({
                           {/* <p className="mb-0">
                             {packages[currentPackage].hk.commission.description}
                           </h5> */}
-                          <p className="mb-0">No Commission on orders below HKD 60,000
-                            Pay <strong className="text-primary">0.03% </strong> per share on orders
-                            above 60,000 HKD
+                          <p className="mb-0">
+                            <strong className="text-primary">$0.002</strong> per share<br/>
+                            <strong className="text-primary">$0.99</strong> per deal minimum
                           </p>
                         </div>
                       </div>
@@ -208,10 +213,10 @@ export function PricePageTemplate({
                             {packages[currentPackage].hk.platformUsageFee.title}
                           </h5>
                         </div>
-                        <hr/>
-                        <div className="col-4 text-right">
+                        <div className="col-6 text-right">
                           <p className="mb-0">
-                            <strong className="text-primary">HKD 18</strong> per deal
+                            <strong className="text-primary">$0.003</strong> per share<br/>
+                            <strong className="text-primary">$1</strong> per deal minimum
                           </p>
                         </div>
                       </div>
@@ -222,9 +227,9 @@ export function PricePageTemplate({
                             {packages[currentPackage].hk.financingInterestRate.title}
                           </h5>
                         </div>
-                        <div className="col-5 text-right">
-                          <p>3.983% minimum</p>
-                        </div>
+                        <p className="col-5 text-right">
+                          <strong className="text-primary">4.2%</strong> minimum
+                        </p>
                       </div>
                     </div>
                   </div>
