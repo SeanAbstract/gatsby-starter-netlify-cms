fa0f4b769d0ebfd3a8365165b3455c86b7e62eea
Sean Urgel <fluttertraining@gmail.com>
2019-11-22 08:49:00 +0800

    fix merge

--- src/templates/contact-page/index.js
+++ src/templates/contact-page/index.js
@@ -274,46 +274,62 @@ export function ContactPageTemplate(props: Props) {
                   </p>
                 </div>
               </div>
-                  <form 
-                    action="/thank-you"
-                    name="contact-form" 
-                    method="post" 
-                    data-netlify="true"
-                    data-netlify-honeypot="bot-field"                  
-                    className="col-md-7 col-11"
-                  >
-                  <input type="hidden" name="bot-field" />
-                  <input type="hidden" name="form-name" value="contact-form" />
-                  <h3 className="mb-3">Leave a message</h3>
-                  <div className="row mb-2">
-                    <div className="col-md-6">
-                      <div className="form-group pt-1">
-                        <label htmlFor="exampleFormControlInput1" style={{fontSize: '14px'}}>
-                          Name
-                        </label>
-                        <input name="Name" type="text" className="form-control" placeholder="John Smith" required />
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group pt-1">
-                        <label htmlFor="exampleFormControlInput1" style={{fontSize: '14px'}}>
-                          Email address
-                        </label>
-                        <input name="Email" type="email" className="form-control" placeholder="jon@snowball.com" required />
-                      </div>
+              <form
+                action="/thank-you"
+                name="contact-form"
+                method="post"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                className="col-md-7 col-11"
+              >
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="contact-form" />
+                <h3 className="mb-3">Leave a message</h3>
+                <div className="row mb-2">
+                  <div className="col-md-6">
+                    <div className="form-group pt-1">
+                      <label htmlFor="exampleFormControlInput1" style={{fontSize: '14px'}}>
+                        Name
+                      </label>
+                      <input
+                        name="Name"
+                        type="text"
+                        className="form-control"
+                        placeholder="John Smith"
+                        required
+                      />
                     </div>
                   </div>
-                  <div className="row mb-2">
-                    <div className="col-md-12">
-                      <div className="form-group pt-1">
-                        <label name="Message" htmlFor="exampleFormControlTextarea1" style={{fontSize: '14px'}}>
-                          Message
-                        </label>
-                        <textarea className="form-control" rows="3" placeholder="Hello..." />
-                      </div>
+                  <div className="col-md-6">
+                    <div className="form-group pt-1">
+                      <label htmlFor="exampleFormControlInput1" style={{fontSize: '14px'}}>
+                        Email address
+                      </label>
+                      <input
+                        name="Email"
+                        type="email"
+                        className="form-control"
+                        placeholder="jon@snowball.com"
+                        required
+                      />
                     </div>
                   </div>
                 </div>
+                <div className="row mb-2">
+                  <div className="col-md-12">
+                    <div className="form-group pt-1">
+                      <label
+                        name="Message"
+                        htmlFor="exampleFormControlTextarea1"
+                        style={{fontSize: '14px'}}
+                      >
+                        Message
+                      </label>
+                      <textarea className="form-control" rows="3" placeholder="Hello..." />
+                    </div>
+                  </div>
+                </div>
+
                 <div className="row mb-2">
                   <div className="col-md-12">
                     <div className="form-group pt-1">
